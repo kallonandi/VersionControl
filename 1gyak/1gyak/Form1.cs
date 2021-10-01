47a6a3f1aacdc636ae454fbe31a7ab7b71035220
kallonandi <nandor.kallo@stud.uni-corvinus.hu>
2021-10-01 11:02:57 +0200

    Refactor name handling

--- 1gyak/1gyak/Form1.cs
+++ 1gyak/1gyak/Form1.cs
@@ -20,8 +20,8 @@ namespace _1gyak
         public Form1()
         {
             InitializeComponent();
-            label1.Text = Resource1.LastName;
-            label2.Text = Resource1.FirstName;
+            label1.Text = Resource1.FullName;
+            
             button1.Text = Resource1.Add;
 
             listBox1.DataSource = users;
@@ -35,8 +35,8 @@ namespace _1gyak
         {
             var u = new User()
             {
-                LastName = textBox1.Text,
-                FirstName = textBox2.Text
+                FullName = textBox1.Text,
+                
             };
             users.Add(u);
         }
