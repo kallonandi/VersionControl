03e6e7801bc7321d62f64538841fa7fa4774405a
kallonandi <nandor.kallo@stud.uni-corvinus.hu>
2021-10-08 11:26:17 +0200

    Create ExcelTable

--- Excelexport/1gyak/Form1.cs
+++ Excelexport/1gyak/Form1.cs
@@ -17,6 +17,8 @@ namespace _1gyak
     public partial class Form1 : Form
 
     {
+        private int _million = (int)Math.Pow(10, 6);
+
         RealEstateEntities context = new RealEstateEntities();
         List<Flat> Flats;
         Excel.Application xlApp;
@@ -67,7 +69,71 @@ namespace _1gyak
 
         private void CreateTable()
         {
+            string[] headers = new string[]
+            {
+                  "Kód",
+                 "Eladó",
+                 "Oldal",
+                 "Kerület",
+                 "Lift",
+                 "Szobák száma",
+                 "Alapterület (m2)",
+                 "Ár (mFt)",
+                 "Négyzetméter ár (Ft/m2)"
 
+            };
+            for (int i = 0; i < headers.Length; i++)
+            {
+                xlSheet.Cells[1, i+1] = headers[i];
+            }
+
+            object[,] values = new object[Flats.Count,headers.Length];
+            int counter = 0;
+            int floorColumn = 6;
+            foreach (var f in Flats)
+            {
+                values[counter, 0] = f.Code;
+                values[counter, 1] = f.Vendor;
+                values[counter, 2] = f.Side;
+                values[counter, 3] = f.District;
+                if (f.Elevator == true)
+                {
+                    values[counter, 4] = "Van";
+                }
+                else
+                {
+                    values[counter, 4] = "nincs";
+                }
+                values[counter, 4] = f.Elevator;
+                values[counter, 5] = f.NumberOfRooms;
+                values[counter, 6] = f.FloorArea;
+                values[counter, 7] = f.Price;
+                values[counter, 8] = string.Format("={0}/{1}*{2}",
+                    "H"+ (counter+2).ToString(),
+                    GetCell(counter+2, floorColumn+1),
+                    _million.ToString());
+                counter++;
+            }
+
+
+            
+
+        }
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
         }
 
 
