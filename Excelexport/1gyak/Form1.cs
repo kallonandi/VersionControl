ce5043ad461e79e0a4b9cc593029c8ba33832742
kallonandi <nandor.kallo@stud.uni-corvinus.hu>
2021-10-08 10:47:37 +0200

    CreateExcel

--- Excelexport/1gyak/Form1.cs
+++ Excelexport/1gyak/Form1.cs
@@ -19,20 +19,57 @@ namespace _1gyak
     {
         RealEstateEntities context = new RealEstateEntities();
         List<Flat> Flats;
+        Excel.Application xlApp;
+        Excel.Workbook xlWB;
+        Excel.Worksheet xlSheet;
 
         public Form1()
         {
             InitializeComponent();
             LoadData();
             dataGridView1.DataSource = Flats;
-
+            CreateExcel();
 
 
         }
-        private void LoadData()
+        public void LoadData()
         {
             Flats = context.Flat.ToList();
         }
+
+        public void CreateExcel()
+        {
+            try
+            {
+                xlApp = new Excel.Application();
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+                xlSheet = xlWB.ActiveSheet;
+
+                CreateTable();
+
+
+
+
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (System.Exception ex)
+            {
+                string hiba = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(hiba, "error");
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
+           
+        }
+
+        private void CreateTable()
+        {
+
+        }
+
 
         private void Form1_Load(object sender, EventArgs e)
         {
