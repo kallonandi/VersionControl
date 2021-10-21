a850d15ed17cfd99a888025366f366493ae5b8f2
kallonandi <nandor.kallo@stud.uni-corvinus.hu>
2021-10-21 20:44:57 +0200

    cleaning

--- 1gyak/1gyak/Form1.Designer.cs
+++ 1gyak/1gyak/Form1.Designer.cs
@@ -28,81 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // listBox1
-            // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(13, 13);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(205, 308);
-            this.listBox1.TabIndex = 0;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(239, 13);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 17);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(314, 13);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(142, 22);
-            this.textBox1.TabIndex = 3;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(242, 99);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(214, 23);
-            this.button1.TabIndex = 5;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(242, 155);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(214, 23);
-            this.button2.TabIndex = 6;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(242, 203);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(214, 23);
-            this.button3.TabIndex = 7;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
-            this.ClientSize = new System.Drawing.Size(468, 362);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.listBox1);
+            this.ClientSize = new System.Drawing.Size(536, 374);
             this.Name = "Form1";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -111,12 +43,6 @@
         private System.Windows.Forms.DataGridView dgw;
         private System.Windows.Forms.Button btnSave;
         private System.Windows.Forms.Button btnLoad;
-        private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
     }
 }
 
--- 1gyak/1gyak/Form1.cs
+++ 1gyak/1gyak/Form1.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
-using System.Data.Entity;
+//using System.Data.Entity;
 using System.Drawing;
 using System.IO;
 using System.Linq;
@@ -15,53 +15,18 @@ namespace _1gyak
 {
     public partial class Form1 : Form
     {
-        BindingList<User> users = new BindingList<User>();
-        
+       
+
         public Form1()
         {
             InitializeComponent();
-            label1.Text = Resource1.FullName;
-            button2.Text = Resource1.WriteToFile;
-            button1.Text = Resource1.Add;
-            button3.Text = Resource1.Delete;
-
-            listBox1.DataSource = users;
-            listBox1.ValueMember = "ID";
-            listBox1.DisplayMember = "FullName";
-
-            
         }
+       
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            var u = new User()
-            {
-                FullName = textBox1.Text,
-                
-            };
-            users.Add(u);
-        }
+      
 
-        private void button2_Click(object sender, EventArgs e)
-        {
-            var sfd = new SaveFileDialog();
-            if (sfd.ShowDialog() != DialogResult.OK)
-                return;
-            using (var sw = new StreamWriter(sfd.FileName, false, Encoding.UTF8)) 
-            {
-                foreach (var u in users)
-                {
-                    sw.WriteLine(string.Format(
-                        "{0};{1}", u.ID, u.FullName));
-                }
-            }
-            
-            
-        }
 
-        private void button3_Click(object sender, EventArgs e)
-        {
-            
-        }
+        
+
     }
 }
