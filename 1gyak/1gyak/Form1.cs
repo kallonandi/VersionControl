4f66d8b9c8430e9b153e1dea48a0ead595452a99
kallonandi <nandor.kallo@stud.uni-corvinus.hu>
2021-10-29 10:43:24 +0200

    charting

--- 1gyak/1gyak/Form1.cs
+++ 1gyak/1gyak/Form1.cs
@@ -10,7 +10,9 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.UI.DataVisualization.Charting;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml;
 
 namespace _1gyak
@@ -25,7 +27,27 @@ namespace _1gyak
             LoadXml(xmlstring);
 
             dataGridView1.DataSource = Rates;
+            Charting();
         }
+
+        private void Charting()
+        {
+            chartRateData.DataSource = Rates;
+            Series series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
+        }
+
         string Consumme()
         {
             MNBArfolyamServiceSoapClient mnbService = new MNBArfolyamServiceSoapClient();
