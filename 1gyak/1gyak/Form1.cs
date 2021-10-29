cf86d6040dd4c9aaec17eb953a1c7628c50adaa5
kallonandi <nandor.kallo@stud.uni-corvinus.hu>
2021-10-29 10:30:03 +0200

    XML feldolgozás

--- 1gyak/1gyak/Form1.cs
+++ 1gyak/1gyak/Form1.cs
@@ -11,18 +11,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace _1gyak
 {
     public partial class Form1 : Form
     {
-
+        BindingList<RateData> Rates = new BindingList<RateData>();
         public Form1()
         {
             InitializeComponent();
-            Consumme();
+            string xmlstring =Consumme();
+            LoadXml(xmlstring);
+
+            dataGridView1.DataSource = Rates;
         }
-        void Consumme()
+        string Consumme()
         {
             MNBArfolyamServiceSoapClient mnbService = new MNBArfolyamServiceSoapClient();
             GetExchangeRatesRequestBody request = new GetExchangeRatesRequestBody();
@@ -31,8 +35,26 @@ namespace _1gyak
             request.endDate = "2020-06-30";
             var response = mnbService.GetExchangeRates(request);
             string result = response.GetExchangeRatesResult;
+            return result;
             
 
+        }
+        private void LoadXml(string input)
+        {
+            XmlDocument xml = new XmlDocument();
+            xml.LoadXml(input);
+            foreach (XmlElement item in xml.DocumentElement)
+            {
+                RateData r = new RateData();
+                r.Date = DateTime.Parse(item.GetAttribute("date"));
+                XmlElement child = (XmlElement)item.FirstChild;
+                r.Currency = child.GetAttribute("curr");
+                r.Value = decimal.Parse(child.InnerText);
+                int unit = int.Parse(child.GetAttribute("unit"));
+                if (unit != 0)
+                    r.Value = r.Value / unit;
+                Rates.Add(r);
+            }
         }
     }
 }
