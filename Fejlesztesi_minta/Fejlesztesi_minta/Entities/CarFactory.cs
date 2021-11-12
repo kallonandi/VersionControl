a885dbc022a7f27ead88dcd93c109b5923d43510
kallonandi <nandor.kallo@stud.uni-corvinus.hu>
2021-11-12 09:59:58 +0100

    use abstraction

--- Fejlesztesi_minta/Fejlesztesi_minta/Entities/CarFactory.cs
+++ Fejlesztesi_minta/Fejlesztesi_minta/Entities/CarFactory.cs
@@ -7,13 +7,11 @@ using System.Threading.Tasks;
 
 namespace Fejlesztesi_minta.Entities
 {
-    public class BallFactory : IToyFactory
+    class CarFactory : IToyFactory
     {
         public Toy CreateNew()
         {
-            return new Ball();
+            return new Car();
         }
-
-        
     }
 }
