d3f1ba68cbdb6b169e0476506086ccb3d2ff2316
kallonandi <nandor.kallo@stud.uni-corvinus.hu>
2021-12-03 10:03:45 +0100

    objectum generation

---
+++ Mikrosimulation/Mikrosimulation/Entities/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mikrosimulation.Entities
+{
+    public class Person
+    {
+        public int BirthYear { get; set; }
+        public Gender Gener { get; set; }
+        public int NbrofChildren { get; set; }
+        public bool IsAlive { get; set; }
+
+        public Person()
+        {
+            IsAlive = true;
+        }
+    }
+}
