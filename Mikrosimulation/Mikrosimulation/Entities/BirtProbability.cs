be0a9f26fd5506fcbb93a22aefa19329e325d302
kallonandi <nandor.kallo@stud.uni-corvinus.hu>
2021-12-03 10:07:38 +0100

    Death/Birth

---
+++ Mikrosimulation/Mikrosimulation/Entities/BirtProbability.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mikrosimulation.Entities
+{
+    public class Birthprobability
+    {
+        public int Age { get; set; }
+ 
+        public int NbrofChildren { get; set; }
+        public double P { get; set; }
+
+    }
+}
