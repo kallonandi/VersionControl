b2e5a2ad117af43a5568db9cef8bc945a33ac1ca
kallonandi <nandor.kallo@stud.uni-corvinus.hu>
2021-12-10 22:26:28 +0100

    starting population

--- Evolution algorythm/Evolution algorythm/Form1.cs
+++ Evolution algorythm/Evolution algorythm/Form1.cs
@@ -15,6 +15,11 @@ namespace Evolution_algorythm
     {
         GameController gc = new GameController();
         GameArea ga;
+        int populationSize = 100;
+        int nbrOfSteps = 10;
+        int nbrOfStepsIncrement = 10;
+        int generation = 1;
+
 
         public Form1()
         {
