9a9de3cf1443d4a1e2a6d07349517e9d66fff1c3
kallonandi <nandor.kallo@stud.uni-corvinus.hu>
2021-12-10 22:28:48 +0100

    new generation

--- Evolution algorythm/Evolution algorythm/Form1.cs
+++ Evolution algorythm/Evolution algorythm/Form1.cs
@@ -42,7 +42,25 @@ namespace Evolution_algorythm
             label1.Text = string.Format(
                 "{0}. generáció",
                 generation);
+            gc.ResetCurrentLevel();
+            var playerList = from p in gc.GetCurrentPlayers()
+                             orderby p.GetFitness() descending
+                             select p;
+            var topPerformers = playerList.Take(populationSize / 2).ToList();
+            foreach (var p in topPerformers)
+            {
+                var b = p.Brain.Clone();
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b);
 
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.Mutate().ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b.Mutate());
+            }
+            gc.Start();
         }
 
     }
